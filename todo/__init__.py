db7033e4ca53be714f17b1f893ed1a11020a6f8c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-06-01 09:10:42 +0200

    Enable adding todos

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
+todos = [
+    'HTML erklären',
+    'Flask / HTTP erklären',
+    'CSS / Bootstrap erklären',
+    'Datenbanken / ORM erklären',
+    'Javascript / Vue erklären',
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', todos=todos)
 
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    todos.append(request.form['todo'])
+    return redirect('/')
